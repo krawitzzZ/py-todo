bd4ab2882562ded846f1ff1845243db4a4ace770
nikita <krawitzzz@hotmail.com>
2017-02-21 23:16:03 +0300

    refactored user serializer

--- todos/serializers.py
+++ todos/serializers.py
@@ -16,11 +16,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
   class Meta:
     model = User
-    fields = ('url', 'id', 'username', 'email', 'password', 'first_name',
-              'last_name', 'date_joined', 'is_active', 'todos',)
-
-  todos = serializers.HyperlinkedRelatedField(many=True, read_only=True,
-                                              view_name='todo-detail')
+    fields = ('url', 'id', 'username', 'email', 'password',
+              'first_name', 'last_name', 'date_joined', 'is_active',)
 
   def create(self, validated_data):
     user = super(UserSerializer, self).create(validated_data=validated_data)
@@ -35,15 +32,4 @@ class TodoSerializer(serializers.HyperlinkedModelSerializer):
     fields = ('url', 'id', 'created', 'updated',
               'owner', 'title', 'description', 'completed',)
 
-  owner = UserSerializer(read_only=True, exclude=('password', 'todos',),)
-
-  # return custom fields by serializer
-  # __hidden_fields field is required in serializer
-  #
-  # def get_fields(self):
-  #   fields = super(UserSerializer, self).get_fields()
-  #
-  #   if self.context.get('view').action != 'create':
-  #     for field in self.__hidden_fields:
-  #       fields.pop(field, None)
-  #   return fields
+  owner = UserSerializer(read_only=True, exclude=('password', 'todos',), )
